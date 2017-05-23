4572062bed1159b39a376e1ad3e9692e34ed955b
Filip Słomski <fslomski@stxnext.pl>
2017-05-23 10:54:50 +0200

    Create another steps file

--- steps/steps.py
+++ steps/steps.py
@@ -3,11 +3,3 @@ class BaseSteps(object):
     @given(u'a user visits the site')
     def impl(context):
         context.login_page.open()
-
-    @when(u'I log in as {username}')
-    def step_impl(context, username):
-        context.login_page.login(username, "1234")
-
-    @then(u'I should see the message {auth_message}')
-    def imple(context, auth_message):
-        assert context.login_page.get_login_message() == auth_message
